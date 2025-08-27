bc6cf2566d08314795ecdf7ec28bc613b8ce575f
chaitanya ponnada <121609540+chaitanyaponnada@users.noreply.github.com>
2025-08-27 14:58:45 +0530

    Updated coin market APIs

--- crypto_price_app.py
+++ crypto_price_app.py
@@ -2,34 +2,32 @@ import streamlit as st
 from PIL import Image
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import base64
-from bs4 import BeautifulSoup
 import requests
-import json
-from cryptocmd import CmcScraper
-import datetime as dt
-
+import base64
+import os
 
 # Set Page to expand to full width
 st.set_page_config(layout="wide")
 
 # Image
-image = Image.open('logo.png')
-st.image(image, width=500)
+try:
+    image = Image.open('logo.png')
+    st.image(image, width=500)
+except FileNotFoundError:
+    st.warning("Logo image ('logo.png') not found. Skipping image display.")
 
 # Title
 st.title('Crypto Web App')
 st.markdown("""
-This app retrieves prices along with other information regarding different cryptocurrencies from **CoinMarketCap**!
+This app retrieves prices along with other information regarding different cryptocurrencies from **CoinMarketCap** using their API!
 """)
 
 # About
 expander_bar = st.expander('About')
 expander_bar.markdown("""
 * **Made By:** <Your Name>
-* **Data source:** [CoinMarketCap](http://coinmarketcap.com).
-* **Credit:** Web scraper adapted from the Medium article *[Web Scraping Crypto Prices With Python](https://towardsdatascience.com/web-scraping-crypto-prices-with-python-41072ea5b5bf)* written by [Bryan Feng](https://medium.com/@bryanf).
+* **Data source:** [CoinMarketCap API](https://coinmarketcap.com/api/documentation/v1/).
+* **Credit:** Adapted from original scraping-based app.
 """)
 
 # Divide Page into columns
@@ -42,62 +40,76 @@ col1.header('Input Options')
 currency_price_unit = col1.selectbox('Select currency for price',
                                      ('USD', 'BTC'))
 
+# API Key
+API_KEY = '863315fd-0f70-4411-892c-44cf6fc86706'
+BASE_URL = 'https://pro-api.coinmarketcap.com'
+
+headers = {
+    'Accept': 'application/json',
+    'X-CMC_PRO_API_KEY': API_KEY
+}
+
 #---------------------------------#
 
-def load_data():
-    cmc = requests.get('https://coinmarketcap.com')
-    soup = BeautifulSoup(cmc.content, 'html.parser')
+def load_data(currency=currency_price_unit):
+    try:
+        # Fetch listings
+        listings_url = f'{BASE_URL}/v1/cryptocurrency/listings/latest'
+        params = {
+            'start': 1,
+            'limit': 100,  # Top 100 coins
+            'convert': currency
+        }
+        response = requests.get(listings_url, headers=headers, params=params)
+        response.raise_for_status()  # Raise exception for bad status codes
+        data = response.json()['data']
 
-    data = soup.find('script', id='__NEXT_DATA__', type='application/json')
-    coin_data = json.loads(data.contents[0])
+        coin_name = []
+        coin_symbol = []
+        market_cap = []
+        percent_change_1h = []
+        percent_change_24h = []
+        percent_change_7d = []
+        price = []
+        volume_24h = []
 
-    global_metrics = coin_data['props']['pageProps']['globalMetrics']
-    total_marketcap = global_metrics['marketCap']
-    btc_market_share = global_metrics['btcDominance']
-    eth_market_share = global_metrics['ethDominance']
+        for coin in data:
+            coin_name.append(coin['name'])
+            coin_symbol.append(coin['symbol'])
+            quote = coin['quote'][currency]
+            price.append(quote['price'])
+            percent_change_1h.append(quote['percent_change_1h'])
+            percent_change_24h.append(quote['percent_change_24h'])
+            percent_change_7d.append(quote['percent_change_7d'])
+            market_cap.append(quote['market_cap'])
+            volume_24h.append(quote['volume_24h'])
 
-    initialState = coin_data['props']['initialState']
-    latest_listing_data = json.loads(initialState)['cryptocurrency']['listingLatest']['data']
-    
-    
-    keysArr = latest_listing_data[0]['keysArr']
-    listings = latest_listing_data[1:]
+        df = pd.DataFrame({
+            'coin_name': coin_name,
+            'coin_symbol': coin_symbol,
+            'market_cap': market_cap,
+            'percent_change_1h': percent_change_1h,
+            'percent_change_24h': percent_change_24h,
+            'percent_change_7d': percent_change_7d,
+            'price': price,
+            'volume_24h': volume_24h
+        })
 
-    query = 'quote.' + currency_price_unit + '.'
+        # Fetch global metrics
+        global_url = f'{BASE_URL}/v1/global-metrics/quotes/latest'
+        global_params = {'convert': 'USD'}
+        global_response = requests.get(global_url, headers=headers, params=global_params)
+        global_response.raise_for_status()
+        global_data = global_response.json()['data']
 
-    coin_name = []
-    coin_symbol = []
-    market_cap = []
-    percent_change_1h = []
-    percent_change_24h = []
-    percent_change_7d = []
-    price = []
-    volume_24h = []
+        total_marketcap = global_data['quote']['USD']['total_market_cap']
+        btc_market_share = global_data['btc_dominance']
+        eth_market_share = global_data['eth_dominance']
 
-    for i in listings:
-
-      i = {key : value for key, value in zip(keysArr, i)}
-
-      coin_name.append(i['slug'])
-      coin_symbol.append(i['symbol'])
-      price.append(i[query+'price'])
-      percent_change_1h.append(i[query+'percentChange1h'])
-      percent_change_24h.append(i[query+'percentChange24h'])
-      percent_change_7d.append(i[query+'percentChange7d'])
-      market_cap.append(i[query+'marketCap'])
-      volume_24h.append(i[query+'volume24h'])
-
-    df = pd.DataFrame(columns=['coin_name', 'coin_symbol', 'market_cap', 'percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price', 'volume_24h'])
-    df['coin_name'] = coin_name
-    df['coin_symbol'] = coin_symbol
-    df['price'] = price
-    df['percent_change_1h'] = percent_change_1h
-    df['percent_change_24h'] = percent_change_24h
-    df['percent_change_7d'] = percent_change_7d
-    df['market_cap'] = market_cap
-    df['volume_24h'] = volume_24h
-
-    return df, total_marketcap, btc_market_share, eth_market_share
+        return df, total_marketcap, btc_market_share, eth_market_share
+    except requests.exceptions.RequestException as e:
+        st.error(f"Failed to fetch data from CoinMarketCap API: {e}")
+        return pd.DataFrame(), 0, 0, 0
 
 df, total_marketcap, btc_market_share, eth_market_share = load_data()
 
@@ -143,7 +155,10 @@ col2.subheader(f'Bar plot of % Price Change')
 col2.write(f'*Last {percent_timeframe} period*')
 
 # Plotting Horizontal Bar Chart
-plt.style.use('seaborn')
+try:
+    plt.style.use('seaborn-v0_8')  # Updated style for Matplotlib 3.6+
+except ValueError:
+    plt.style.use('default')  # Fallback to default style if seaborn-v0_8 unavailable
 
 fig, ax = plt.subplots()
 ax.barh(bar_chart_df['coin_symbol'],
@@ -163,21 +178,21 @@ col2.pyplot(fig)
 
 def get_unit(max_market_cap):
   
-  unit = 'less than ten million'
-  number_of_digits = len(str(int(max_market_cap)))
+    unit = 'less than ten million'
+    number_of_digits = len(str(int(max_market_cap)))
 
-  if number_of_digits == 8:
-    unit = 'tens of millions'
-  elif number_of_digits == 9:
-    unit = 'hundreds of millions'
-  elif number_of_digits == 10:
-    unit = 'billions'
-  elif number_of_digits == 11:
-    unit = 'tens of billions'
-  elif number_of_digits == 12:
-    unit = 'hundreds of billions'
+    if number_of_digits == 8:
+        unit = 'tens of millions'
+    elif number_of_digits == 9:
+        unit = 'hundreds of millions'
+    elif number_of_digits == 10:
+        unit = 'billions'
+    elif number_of_digits == 11:
+        unit = 'tens of billions'
+    elif number_of_digits == 12:
+        unit = 'hundreds of billions'
   
-  return unit
+    return unit
 
 # Heading for Bar Chart
 col3.subheader(f'Bar plot of Market Cap (Selected Cryptos)')
@@ -189,7 +204,7 @@ ax.bar(selected_coin_df['coin_symbol'],
         selected_coin_df['market_cap'])
 ax.tick_params(axis='both', labelsize=15)
 
-# Increasing size of exponenet
+# Increasing size of exponent
 exponent = ax.yaxis.get_offset_text()
 exponent.set_size(16)
 
@@ -232,53 +247,6 @@ col2.pyplot(fig)
 
 #---------------------------------#
 
-col3.markdown("""
-_________________________
-""")
-
-# Getting Historical Time Series Data
-# Using cryptocmd - Cryptocurrency historical market price data scrapper
-@st.cache(max_entries=25, ttl=86400)
-def get_historical_time_series_data(crypto_symbol, startdate, endate):
-
-  # initialise scraper with time interval
-  scraper = CmcScraper(crypto_symbol, startdate, endate)
-
-  # get dataframe for the data
-  historical_timeseries_df = scraper.get_dataframe()
-
-  return historical_timeseries_df
-
-# Select crypto
-selected_crypto = col3.selectbox('Select crypto', (df['coin_symbol']))
-
-# Heading for Line Graph
-col3.header(f'{selected_crypto} over the last 30 days')
-
-today = dt.date.today()
-month_ago = today - dt.timedelta(days=30)
-
-# Convert to '%d-%m-%Y' string format
-today = today.strftime('%d-%m-%Y')
-month_ago = month_ago.strftime('%d-%m-%Y')
-
-historical_timeseries_df =\
-   get_historical_time_series_data(selected_crypto, month_ago, today)         
-
-# Plot Line Graph
-fig, ax = plt.subplots()
-ax.plot(historical_timeseries_df['Date'],
-       historical_timeseries_df['Close'], color='green')
-
-ax.set_xlabel('Date', fontsize=15, labelpad=13)
-ax.set_ylabel('Closing Price ($)', fontsize=15, labelpad=15)
-ax.tick_params(axis='x', rotation=45)
-
-# Display figure
-col3.pyplot(fig)
-
-#---------------------------------#
-
 col2.markdown("""
 _________________________
 """)
@@ -286,8 +254,6 @@ _________________________
 col2.header('**Tables**')
 
 # Price Data Table
-
-# Select columns
 columns = ['coin_name', 'coin_symbol', 'market_cap', 'price', 'volume_24h']
 selected_coin_price_info_df = selected_coin_df[columns]
 
@@ -295,7 +261,6 @@ col2.subheader('Price Data of Selected Cryptocurrencies')
 col2.write(selected_coin_price_info_df)
 
 # Download CSV data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
@@ -306,10 +271,8 @@ col2.markdown(filedownload(selected_coin_price_info_df),
               unsafe_allow_html=True)
 
 # Table of Percentage Change
-
-# Drop columns not related to Percentage Change
-selected_coin_percent_change_df =\
-   selected_coin_df.drop(columns=['market_cap', 'price', 'volume_24h'])
+selected_coin_percent_change_df = \
+    selected_coin_df.drop(columns=['market_cap', 'price', 'volume_24h'])
 
 col2.subheader('Percent Change Data of Select Cryptocurrencies')
 col2.write(selected_coin_percent_change_df)
